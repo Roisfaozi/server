3ebfda8c5f4fab4f1520362fe53ad023f0110077
Roisfaozi <roisfaozi55@gmail.com>
2024-02-21 16:06:14 +0700

    *Feat: Add auth method for authetication users

---
+++ src/controllers/auth.js
@@ -0,0 +1,61 @@
+const userModel = require('../models/users');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const registerUser = async (req, res) => {
+  const { username, password, email, role = 'user' } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(req.body);
+    const newUser = await userModel.addUser(
+      username,
+      hashedPassword,
+      email,
+      role
+    );
+
+    const token = generateToken(newUser);
+
+    res.json({ token, user: newUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await userModel.getUserByUsename(username);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = generateToken(user);
+
+      res.json({ token, user });
+    } else {
+      res.status(401).json({ error: 'Invalid credentials' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const generateToken = (user) => {
+  const payload = {
+    userId: user.user_id,
+    username: user.username,
+    role: user.role,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+  return token;
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
