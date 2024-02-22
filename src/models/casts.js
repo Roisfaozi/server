203c5d4cd341c0b08358155a6ec2d82f432b1204
Roisfaozi <roisfaozi55@gmail.com>
2024-02-22 08:13:38 +0700

    *feat: add exist validation check in adCast

--- src/models/casts.js
+++ src/models/casts.js
@@ -3,11 +3,25 @@ const db = require('../config/dbConfig');
 const models = {
   addCast: async (cast_name) => {
     try {
-      const result = await db.query(
-        'INSERT INTO casts (name) VALUES ($1) RETURNING id;',
-        [cast_name]
-      );
-      return result.rows[0].id;
+      const checkIfCastExists = async (cast_name) => {
+        const queryResult = await db.query(
+          'SELECT id FROM casts WHERE name = $1;',
+          [cast_name]
+        );
+        return queryResult.rows.length > 0;
+      };
+
+      const castExists = await checkIfCastExists(cast_name);
+
+      if (!castExists) {
+        const result = await db.query(
+          'INSERT INTO casts (name) VALUES ($1) RETURNING id;',
+          [cast_name]
+        );
+        return result.rows[0].id;
+      } else {
+        return new Error('Cast name already exists');
+      }
     } catch (error) {
       throw new Error(`Error creating director: ${error.message}`);
     }
