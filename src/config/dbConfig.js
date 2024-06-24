cedc321133ebe377c63bcf8431451211b8a62465
Roisfaozi <roisfaozi55@gmail.com>
2024-06-24 07:55:02 +0700

    update docker compose

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
+  ssl: true,
   max: 10,
   min: 0,
   idleTimeoutMillis: 8000,
